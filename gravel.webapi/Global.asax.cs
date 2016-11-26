b569cf2ec15a5ebcea0d76a8be3182b766f4dbd1
Grant <lianghungdi@gmail.com>
2016-11-27 00:49:16 +0800

    fixed for mac

--- gravel.webapi/Global.asax.cs
+++ gravel.webapi/Global.asax.cs
@@ -14,12 +14,11 @@ namespace gravel.webapi
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
          
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
